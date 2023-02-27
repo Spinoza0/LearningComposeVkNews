1db3ce14c9fb4269eeefb27e90cbf62935f971b5
Spinoza0 <58294846+Spinoza0@users.noreply.github.com>
2023-02-27 15:17:58 +0300

    add HomeScreen.kt

--- app/src/main/java/com/spinoza/learningvknews/presentation/ui/VkNewsMainScreen.kt
+++ app/src/main/java/com/spinoza/learningvknews/presentation/ui/VkNewsMainScreen.kt
@@ -1,85 +1,47 @@
 package com.spinoza.learningvknews.presentation.ui
 
-import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
+import com.spinoza.learningvknews.presentation.HomeScreen
 import com.spinoza.learningvknews.presentation.viewmodel.MainViewModel
 
-@OptIn(ExperimentalMaterialApi::class, ExperimentalFoundationApi::class)
 @Composable
 fun MainScreen(viewModel: MainViewModel) {
 
+    val selectedNavItem by viewModel.selectedNavItem.observeAsState(NavigationItem.Home)
+
     Scaffold(
-
-        bottomBar = { MainScreenBottomBar() }
-    ) { padding ->
-        val feedPosts = viewModel.feedPosts.observeAsState(listOf())
-
-        LazyColumn(
-            modifier = Modifier.padding(padding),
-            contentPadding = PaddingValues(
-                top = 16.dp,
-                start = 8.dp,
-                end = 8.dp,
-                bottom = 72.dp
-            ),
-            verticalArrangement = Arrangement.spacedBy(8.dp)
-        ) {
-            items(feedPosts.value, key = { it.id }) { feedPost ->
-                val dismissState = rememberDismissState()
-
-                if (dismissState.isDismissed(DismissDirection.EndToStart) ||
-                    dismissState.isDismissed(DismissDirection.StartToEnd)
-                ) {
-                    viewModel.delete(feedPost)
-                }
-
-                SwipeToDismiss(
-                    modifier = Modifier.animateItemPlacement(),
-                    state = dismissState,
-                    dismissThresholds = { FractionalThreshold(0.75f) },
-                    background = { },
-                    directions = setOf(DismissDirection.EndToStart, DismissDirection.StartToEnd)
-                ) {
-                    PostCard(
-                        feedPost = feedPost,
-                        onStatisticClickListener = { statisticItem ->
-                            viewModel.updateCount(feedPost, statisticItem)
-                        }
-                    )
-                }
-            }
+        bottomBar = {
+            MainScreenBottomBar(selectedNavItem) { viewModel.selectNavItem(it) }
+        }
+    ) {paddingValues ->
+        when (selectedNavItem) {
+            NavigationItem.Home -> HomeScreen(viewModel, paddingValues)
+            else -> {}
         }
     }
 }
 
 @Composable
-private fun MainScreenBottomBar() {
+private fun MainScreenBottomBar(
+    selectedNavItem: NavigationItem,
+    onSelectItem: (NavigationItem) -> Unit,
+) {
     BottomNavigation {
         val items = listOf(
             NavigationItem.Home,
             NavigationItem.Favourite,
             NavigationItem.Profile
         )
-        val selectedItemPosition = remember {
-            mutableStateOf(FIRST_ITEM)
-        }
-        items.forEachIndexed() { index, item ->
+
+        items.forEach { item ->
             val title = stringResource(item.titleResId)
             BottomNavigationItem(
-                selected = selectedItemPosition.value == index,
-                onClick = { selectedItemPosition.value = index },
+                selected = selectedNavItem == item,
+                onClick = { onSelectItem(item) },
                 icon = { Icon(item.icon, contentDescription = title) },
                 label = { Text(title) },
                 selectedContentColor = MaterialTheme.colors.onPrimary,
@@ -88,5 +50,3 @@ private fun MainScreenBottomBar() {
         }
     }
 }
-
-private const val FIRST_ITEM = 0
--- app/src/main/java/com/spinoza/learningvknews/presentation/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/spinoza/learningvknews/presentation/viewmodel/MainViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.spinoza.learningvknews.domain.FeedPost
 import com.spinoza.learningvknews.domain.StatisticItem
+import com.spinoza.learningvknews.presentation.ui.NavigationItem
 
 class MainViewModel : ViewModel() {
     private val initialList = mutableListOf<FeedPost>().apply {
@@ -15,6 +16,13 @@ class MainViewModel : ViewModel() {
 
     private val _feedPosts = MutableLiveData<List<FeedPost>>(initialList)
     val feedPosts: LiveData<List<FeedPost>> = _feedPosts
+
+    private val _selectedNavItem = MutableLiveData<NavigationItem>(NavigationItem.Home)
+    val selectedNavItem: LiveData<NavigationItem> = _selectedNavItem
+
+    fun selectNavItem(navigationItem: NavigationItem) {
+        _selectedNavItem.value = navigationItem
+    }
 
     fun updateCount(feedPost: FeedPost, statisticItem: StatisticItem) {
         val oldStatistics = feedPost.statistics
