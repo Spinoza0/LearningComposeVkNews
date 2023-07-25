2936db78e2709ed48e40931afe7638eee1bdb800
Spinoza0 <58294846+Spinoza0@users.noreply.github.com>
2023-07-25 19:29:45 +0300

    add login to VK

--- app/src/main/java/com/spinoza/learningvknews/presentation/feature/mainscreen/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/spinoza/learningvknews/presentation/feature/mainscreen/viewmodel/MainViewModel.kt
@@ -2,6 +2,7 @@ package com.spinoza.learningvknews.presentation.feature.mainscreen.viewmodel
 
 import androidx.lifecycle.ViewModel
 import com.spinoza.learningvknews.presentation.feature.mainscreen.model.AuthState
+import com.vk.api.sdk.VK
 import com.vk.api.sdk.auth.VKAuthenticationResult
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -11,13 +12,17 @@ class MainViewModel : ViewModel() {
 
     private val _authState = MutableStateFlow<AuthState>(AuthState.Initial)
 
-    private val authState: StateFlow<AuthState>
+    val authState: StateFlow<AuthState>
         get() = _authState.asStateFlow()
+
+    init {
+        _authState.value = if (VK.isLoggedIn()) AuthState.Authorized else AuthState.NotAuthorized
+    }
 
     fun preformAuthResult(result: VKAuthenticationResult) {
         when (result) {
-                        is VKAuthenticationResult.Success -> {}
-                        is VKAuthenticationResult.Failed -> {}
-                    }
+            is VKAuthenticationResult.Success -> _authState.value = AuthState.Authorized
+            is VKAuthenticationResult.Failed -> _authState.value = AuthState.NotAuthorized
+        }
     }
 }
